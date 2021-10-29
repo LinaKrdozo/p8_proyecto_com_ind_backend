54b56f44d5555cbed81fc9fe21f08c82121cc159
Linakrdozo <linakrdozo13@gmail.com>
2021-10-28 21:48:18 -0500

    last changes

--- authModuleComInd/settings_prod.py
+++ authModuleComInd/settings_prod.py
@@ -30,7 +30,7 @@ DEBUG = False
 ALLOWED_HOSTS = ['ec2-18-214-214-252.compute-1.amazonaws.com', 'localhost']
 CORS_ORIGINS_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = [
-    'ec2-18-214-214-252.compute-1.amazonaws.com'
+    'https://ec2-18-214-214-252.compute-1.amazonaws.com'
 ]
 
 # Application definition
